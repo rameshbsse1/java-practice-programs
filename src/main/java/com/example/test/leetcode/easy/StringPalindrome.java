0c42d570d027114296f2a2642e3f2f2ab127fbb8
Ramesh Bandari <rameshbsse1@gmail.com>
2025-05-11 19:04:46 -0400

    code refactoring

--- src/main/java/com/example/test/leetcode/easy/StringPalindrome.java
+++ src/main/java/com/example/test/leetcode/easy/StringPalindrome.java
@@ -2,13 +2,23 @@ package com.example.test.leetcode.easy;
 
 public class StringPalindrome {
 
-    public static boolean isPolindrome(String str) {
+    public static boolean isPalindrome(String str) {
         str = str.toLowerCase();
         str = str.replaceAll(" ", "");
         int i =0;
         int j =str.length()-1;
 
+
         while(i< j) {
+
+            while(i < j && !Character.isLetterOrDigit(str.charAt(i))){
+                i++;
+            }
+
+            while(i<j && !Character.isLetterOrDigit(str.charAt(j))){
+                j--;
+            }
+
             if (str.charAt(i) != str.charAt(j)) {
                 return false;
             }
@@ -17,8 +27,17 @@ public class StringPalindrome {
         return true;
     }
 
+    public static boolean isPalindromeUsingStringBuilder(String str) {
+        String cleaned = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String st = new StringBuilder(cleaned).reverse().toString();
+        return st.equals(cleaned);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println(StringPalindrome.isPolindrome("level"));
-        System.out.println(StringPalindrome.isPolindrome("Racecar"));
+        System.out.println(StringPalindrome.isPalindrome("level"));
+        System.out.println(StringPalindrome.isPalindrome("Racecar"));
+        System.out.println(StringPalindrome.isPalindromeUsingStringBuilder("A man, a plan, a canal: Panama"));
+        System.out.println(StringPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
