7fd49c87ea25c79514662a24b6a5ec0ddc5ad0bc
Ramesh Bandari <rameshbsse1@gmail.com>
2025-05-25 20:47:35 -0400

    Roman to integer

---
+++ src/main/java/com/example/test/leetcode/easy/RomanToInteger.java
@@ -0,0 +1,32 @@
+package com.example.test.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String str2 = "LVIII";
+        Map<Character, Integer> charMap = new HashMap<>();
+        charMap.put('I', 1);
+        charMap.put('V', 5);
+        charMap.put('X', 10);
+        charMap.put('L', 50);
+        charMap.put('C', 100);
+        charMap.put('D', 500);
+        charMap.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i= str2.length()-1; i>=0; i--) {
+            int current = charMap.get(str2.charAt(i));
+            if (current < prevValue) {
+                total -= current;
+            } else {
+                total += current;
+            }
+            prevValue = current;
+        }
+        System.out.println("total :" +total);
+    }
+}
